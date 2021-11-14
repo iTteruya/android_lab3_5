c3c09f05d69cc04bcc32d99dd5681c71e5d1f25d
iTteruya <timur.ivanov@outlook.com>
2021-11-14 18:23:19 +0300

    added test + refactoring

---
+++ app/src/androidTest/java/com/example/lab3_5/AboutUtils.kt
@@ -0,0 +1,11 @@
+package com.example.lab3_5
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+private fun openAboutViaBottomNav() {
+    onView(withId(R.id.nav_view)).perform(click())
+}
+
+fun openAbout() = openAboutViaBottomNav()
